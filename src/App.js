9ee9b11508b952955d378586be51746124d98288
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-05-17 19:43:59 +0200

    adding cloudinary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
+import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 import logo from './logo.svg';
 import './App.css';
 
+const ImageTransformations = ({width, selectedShirt}) => {
+  return (
+    <Image prublicId={selectedShirt.main +'.jpg'}>
+      <Transformation width={width} crop="scale"/>
+    </Image>
+  );
+};
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    const defaultShirt = {id: 1, main: 'shirt_only'};
+    this.state = {
+      shirts: [
+        defaultShirt,
+        {id: 2, main: 'laying-shirt'},
+        {id: 3, main: 'hanging_t-shirt'}
+      ],
+      selectedShirt: defaultShirt,
+    };
+  }
+
+  selectedShirt(thumb){
+    this.setSatate({ selectedShirt: thumb}, _ => this.forceUpdate())
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CloudinaryContext cloudName="marialobillo-com">
+          <div id="imageDemoContainer">
+            <div id="mainImage">
+              <ImageTransformations
+                width="600"
+                rgb={rgb}
+                selectedShirt={this.state.selectedShirt}
+                text={this.state.text} />
+            </div>
+            <div id="imageThumb">
+              <ul id="thumbs">
+                {this.state.shirts.map(thumbs => {
+                  return (
+                    <li className={thum.main === this.state.selectedShirt.main ? 'active': ''} onClick={this.selectedShirt.bind(this, thumb)} key={thum.id}>
+                    {/*<Image prublicId={thumb.main}>*/}
+                      {/*<Transformation width="75" crop="scale" />*/}
+                    {/*</Image>*/}
+                    <ImageTransformations
+                      width="75"
+                      rgb={rgb}
+                      selectedShirt={thumb}
+                      text={' '} />
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </CloudinaryContext>
       </div>
     );
   }
